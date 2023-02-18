0a6ecc91cf49e56eafc2405746f09da51b23e167
Pedro Lustosa <pedrolustosa99@gmail.com>
2023-02-18 17:17:58 -0300

    microphone-speech-to-text implemantation

---
+++ microphone-speech-to-text.py
@@ -0,0 +1,33 @@
+import speech_recognition as sr
+
+
+# Função para ouvir e reconhecer a fala
+def ouvir_microfone():
+    # Habilita o microfone do usuário
+    microfone = sr.Recognizer()
+
+    # usando o microfone
+    with sr.Microphone() as source:
+
+        # Chama um algoritmo de reducao de ruidos no som
+        microfone.adjust_for_ambient_noise(source)
+
+        # Frase para o usuario dizer algo
+        print("Diga alguma coisa: ")
+
+        # Armazena o que foi dito numa variavel
+        audio = microfone.listen(source)
+
+    try:
+
+        # Passa a variável para o algoritmo reconhecedor de padroes
+        frase = microfone.recognize_google(audio, language='pt-BR')
+
+        # Retorna a frase pronunciada
+        print("Você disse: " + frase)
+
+    # Se nao reconheceu o padrao de fala, exibe a mensagem
+    except sr.UnkownValueError:
+        print("Não entendi")
+
+    return frase
