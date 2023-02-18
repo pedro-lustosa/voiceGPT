7662aad392018ae97934354a5cbafc0b86cc5e49
Pedro Lustosa <pedrolustosa99@gmail.com>
2023-02-18 17:59:51 -0300

    stt changed var names

--- microphone-speech-to-text.py
+++ microphone-speech-to-text.py
@@ -2,32 +2,32 @@ import speech_recognition as sr
 
 
 # Função para ouvir e reconhecer a fala
-def ouvir_microfone():
+def start_microphone():
     # Habilita o microfone do usuário
-    microfone = sr.Recognizer()
+    microphone = sr.Recognizer()
 
     # usando o microfone
     with sr.Microphone() as source:
 
         # Chama um algoritmo de reducao de ruidos no som
-        microfone.adjust_for_ambient_noise(source)
+        microphone.adjust_for_ambient_noise(source)
 
         # Frase para o usuario dizer algo
-        print("Diga alguma coisa: ")
+        print("Listening... ")
 
         # Armazena o que foi dito numa variavel
-        audio = microfone.listen(source)
+        audio = microphone.listen(source)
 
     try:
 
         # Passa a variável para o algoritmo reconhecedor de padroes
-        frase = microfone.recognize_google(audio, language='pt-BR')
+        speech = microphone.recognize_google(audio, language='pt-BR')
 
         # Retorna a frase pronunciada
-        print("Você disse: " + frase)
+        print("You said: " + speech)
 
     # Se nao reconheceu o padrao de fala, exibe a mensagem
     except sr.UnkownValueError:
-        print("Não entendi")
+        print("Didn't understand.")
 
-    return frase
+    return speech
